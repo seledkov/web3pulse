80a2f4256fe549ab5741196faca55ea1c2081baa
Timur <seledkovti@gmail.com>
2020-10-11 19:13:57 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -1,32 +1,8 @@
-/* let test = '6/3=?';
-
-let answer = prompt(test);
-// console.log("her years" answer)
-
-if (3 * answer == 6) {
-	console.log('yes')
-}
-else { 
-	console.log('no no no')
-} */
-alert('Давай проверим как ты умеешь умножать числа');
-let firstNumber = prompt('введите первое число'),
-	secondNumber = prompt('введите второе число');
-
-let answer = firstNumber * secondNumber;
-let userAnswer = prompt('введите ответ');
-if (answer == userAnswer) {
-	alert('верное решение!')
-} else {
-	alert('неверное решение!')
-	let nextStep = confirm('хочешь узнать ответ?');
-	if (nextStep == true) {
-		alert('правильный ответ' + answer)
-	} else {
-		alert('чтобы попробовать еще раз обновите страницу')
-	}
-};
-
-/* for (let i = 1; i < 8; i++) {
-	console.log(i);
-} */
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 500,
+		/* adaptiveHeight: true, */
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png" alt="<" /></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-left.png" alt=">" /></button>'
+	});
+});
---
+++ js/testScript.js
@@ -0,0 +1,32 @@
+/* let test = '6/3=?';
+
+let answer = prompt(test);
+// console.log("her years" answer)
+
+if (3 * answer == 6) {
+	console.log('yes')
+}
+else { 
+	console.log('no no no')
+} */
+alert('Давай проверим как ты умеешь умножать числа');
+let firstNumber = prompt('введите первое число'),
+	secondNumber = prompt('введите второе число');
+
+let answer = firstNumber * secondNumber;
+let userAnswer = prompt('введите ответ');
+if (answer == userAnswer) {
+	alert('верное решение!')
+} else {
+	alert('неверное решение!')
+	let nextStep = confirm('хочешь узнать ответ?');
+	if (nextStep == true) {
+		alert('правильный ответ' + answer)
+	} else {
+		alert('чтобы попробовать еще раз обновите страницу')
+	}
+};
+
+/* for (let i = 1; i < 8; i++) {
+	console.log(i);
+} */
