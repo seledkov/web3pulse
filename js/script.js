4809f7dd76f68793f36a2f3914b8d735cdc145a7
Timur <seledkovti@gmail.com>
2020-10-13 03:10:02 +0300

    add tabs active

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function () {
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png" alt="<" /></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-left.png" alt=">" /></button>'
 	});
+
+	$('ul.catalog__tabs').on('click', 'li:not(catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__contents').removeClass('catalog__contents_active').eq($(this).index()).addClass('catalog__contents_active');
+	});
+
+	$
 });
